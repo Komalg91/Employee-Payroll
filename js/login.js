6533d3e81af126b03ad821c52c305d2ecd337cb4
Komalg91 <kggk9050@gmail.com>
2021-08-26 07:41:52 +0530

    UC14 template literals

---
+++ js/login.js
@@ -0,0 +1,113 @@
+const table_js = document.getElementById("table");
+table_js.innerHTML = `
+<tr class="table_head">
+<th>Name</th>
+<th>Gender</th>
+<th>Department</th>
+<th>Salary</th>
+<th>Start Date</th>
+<th>Actions</th>
+</tr>    <tr>
+<td>
+    <img src="../assets/Ellipse 1.png" alt="profile_img"  class="profile_img">
+    Amarpa ShashankaKeerthi Kumar
+</td>
+<td>Female</td>
+<td>
+    <span class="table_dept">Sales</span>
+    <span class="table_dept">HR</span>
+    <span class="table_dept">Finance</span>
+</td>
+<td>10000</td>
+<td>29 October 2019</td>
+<td>
+    <button type="submit" ><i class="fa fa-trash"></i></button>
+    <button type="submit"><i class="fa fa-edit"></i></button>
+</td>
+</tr><tr>
+<td>
+    <img src="../assets/Ellipse 1.png" alt="profile_img"  class="profile_img">
+    Amarpa ShashankaKeerthi Kumar
+</td>
+<td>Female</td>
+<td>
+    <span class="table_dept">Sales</span>
+    <span class="table_dept">HR</span>
+    <span class="table_dept">Finance</span>
+</td>
+<td>10000</td>
+<td>29 October 2019</td>
+<td>
+    <button type="submit" ><i class="fa fa-trash"></i></button>
+    <button type="submit"><i class="fa fa-edit"></i></button>
+</td>
+</tr><tr>
+<td>
+    <img src="../assets/Ellipse 1.png" alt="profile_img"  class="profile_img">
+    Amarpa ShashankaKeerthi Kumar
+</td>
+<td>Female</td>
+<td>
+    <span class="table_dept">Sales</span>
+    <span class="table_dept">HR</span>
+    <span class="table_dept">Finance</span>
+</td>
+<td>10000</td>
+<td>29 October 2019</td>
+<td>
+    <button type="submit" ><i class="fa fa-trash"></i></button>
+    <button type="submit"><i class="fa fa-edit"></i></button>
+</td>
+</tr><tr>
+<td>
+    <img src="../assets/Ellipse 1.png" alt="profile_img"  class="profile_img">
+    Amarpa ShashankaKeerthi Kumar
+</td>
+<td>Female</td>
+<td>
+    <span class="table_dept">Sales</span>
+    <span class="table_dept">HR</span>
+    <span class="table_dept">Finance</span>
+</td>
+<td>10000</td>
+<td>29 October 2019</td>
+<td>
+    <button type="submit" ><i class="fa fa-trash"></i></button>
+    <button type="submit"><i class="fa fa-edit"></i></button>
+</td>
+</tr><tr>
+<td>
+    <img src="../assets/Ellipse 1.png" alt="profile_img"  class="profile_img">
+    Amarpa ShashankaKeerthi Kumar
+</td>
+<td>Female</td>
+<td>
+    <span class="table_dept">Sales</span>
+    <span class="table_dept">HR</span>
+    <span class="table_dept">Finance</span>
+</td>
+<td>10000</td>
+<td>29 October 2019</td>
+<td>
+    <button type="submit" ><i class="fa fa-trash"></i></button>
+    <button type="submit"><i class="fa fa-edit"></i></button>
+</td>
+</tr><tr>
+<td>
+    <img src="../assets/Ellipse 1.png" alt="profile_img"  class="profile_img">
+    Amarpa ShashankaKeerthi Kumar
+</td>
+<td>Female</td>
+<td>
+    <span class="table_dept">Sales</span>
+    <span class="table_dept">HR</span>
+    <span class="table_dept">Finance</span>
+</td>
+<td>10000</td>
+<td>29 October 2019</td>
+<td>
+    <button type="submit" ><i class="fa fa-trash"></i></button>
+    <button type="submit"><i class="fa fa-edit"></i></button>
+</td>
+</tr>
+ `
